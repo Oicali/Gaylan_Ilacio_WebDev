11931ffc4610f2904022c6a82f104c45cf07b6a2
Oicali <121882893+Oicali@users.noreply.github.com>
2025-05-27 19:04:34 +0800

    almost complete

---
+++ js/products.js
@@ -0,0 +1,162 @@
+const products = [
+    {
+        id: 1,
+        title: "Clamp Bouquet",
+        price: "₱65",
+        description: "A charming mix of hair clamps and flowers, tied with a ribbon of congratulations.",
+        colors: [
+            {
+                name: "Purple",
+                mainImage: "/img/clamp-bouquet/purple-clamp-bouquet.jpg",
+                thumbnails: [
+                    "/img/clamp-bouquet/purple-clamp-bouquet2.jpg",
+                ]
+            },
+            {
+                name: "Red",
+                mainImage: "/img/clamp-bouquet/red-clamp-bouquet.jpg",
+                thumbnails: [
+                    "/img/clamp-bouquet/red-clamp-bouquet2.jpg"
+                ]
+            },
+            {
+                name: "Pink",
+                mainImage: "/img/clamp-bouquet/pink-clamp-bouquet.jpg",
+                thumbnails: [
+                    "/img/clamp-bouquet/pink-clamp-bouquet2.jpg"
+                ]
+            }, 
+            {
+                name: "Blue",
+                mainImage: "/img/clamp-bouquet/blue-clamp-bouquet.jpg",
+                thumbnails: [
+                    "/img/clamp-bouquet/blue-clamp-bouquet2.jpg"
+                ]
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Artificial Tulips Bouquet",
+        price: "$100",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        colors: [
+            {
+                name: "Purple",
+                mainImage: "/img/tulips-bouquet.jpg",
+                thumbnails: [
+                    "/img/clamp-bouquet.jpg",
+                    "/img/clamp-bouquet.jpg",
+                    "/img/clamp-bouquet.jpg"
+                ],
+            },
+            {
+                name: "Pink",
+                mainImage: "/img/clamp-bouquet.jpg",
+                thumbnails: [
+                    
+                ],
+            },
+            {
+                name: "Blue",
+                mainImage: "/img/clamp-bouquet.jpg",
+                thumbnails: [
+                    
+                ],
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Money Bouquet",
+        price: "$100",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        colors: [
+            {
+                name: "Purple",
+                mainImage: "/img/money-bouquet.jpg",
+                thumbnails: [
+                    "/img/clamp-bouquet.jpg",
+                    "/img/clamp-bouquet.jpg",
+                    "/img/clamp-bouquet.jpg"
+                ],
+            },
+            {
+                name: "Pink",
+                mainImage: "/img/clamp-bouquet.jpg",
+                thumbnails: [
+                    
+                ],
+            },
+            {
+                name: "Blue",
+                mainImage: "/img/clamp-bouquet.jpg",
+                thumbnails: [
+                    
+                ],
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Satin Roses Bouquet",
+        price: "$100",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        colors: [
+            {
+                name: "Purple",
+                mainImage: "/img/clamp-bouquet.jpg",
+                thumbnails: [
+                    "/img/clamp-bouquet.jpg",
+                    "/img/clamp-bouquet.jpg",
+                    "/img/clamp-bouquet.jpg"
+                ],
+            },
+            {
+                name: "Pink",
+                mainImage: "/img/clamp-bouquet.jpg",
+                thumbnails: [
+                    
+                ],
+            },
+            {
+                name: "Blue",
+                mainImage: "/img/clamp-bouquet.jpg",
+                thumbnails: [
+                    
+                ],
+            }
+        ]
+    },
+    {
+        id: 5,
+        price: "$100",
+        title: "Butterfly Bouquet",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        colors: [
+            {
+                name: "Purple",
+                mainImage: "/img/clamp-bouquet.jpg",
+                thumbnails: [
+                    "/img/clamp-bouquet.jpg",
+                    "/img/clamp-bouquet.jpg",
+                    "/img/clamp-bouquet.jpg"
+                ],
+            },
+            {
+                name: "Pink",
+                mainImage: "/img/clamp-bouquet.jpg",
+                thumbnails: [
+                    
+                ],
+            },
+            {
+                name: "Blue",
+                mainImage: "/img/clamp-bouquet.jpg",
+                thumbnails: [
+                    
+                ],
+            }
+        ]
+    }
+]
